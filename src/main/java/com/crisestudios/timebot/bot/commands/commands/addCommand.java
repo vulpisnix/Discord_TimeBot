c7eed99ddf5174135b9f2d41b10652247de49dc9
Philipp Schott <schottphilipp0@gmail.com>
2021-07-01 15:59:26 +0200

    Started working on the add command

--- src/main/java/com/crisestudios/timebot/bot/commands/commands/addCommand.java
+++ src/main/java/com/crisestudios/timebot/bot/commands/commands/addCommand.java
@@ -1,5 +1,6 @@
 package com.crisestudios.timebot.bot.commands.commands;
 
+import com.crisestudios.timebot.bot.Bot;
 import com.crisestudios.timebot.bot.commands.ICommandC;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
@@ -20,6 +21,12 @@ public class addCommand extends ICommandC {
 
     @Override
     public void execute(Guild guild, Member author, Message message, TextChannel channel, String[] args) {
-        channel.sendMessage("Conner stinkt").queue();
+        if(args.length < 3 || message.getMentionedRoles().size() < 1) {
+            channel.sendMessage("**Usage:** "+getUsage().replaceAll("%prefix%", Bot.getCommandPrefix())).queue();
+            return;
+        }
+        String timeRaw = args[1];
+
+        System.out.println(timeRaw);
     }
 }
--- src/main/java/com/crisestudios/timebot/bot/events/BotReadyEvent.java
+++ src/main/java/com/crisestudios/timebot/bot/events/BotReadyEvent.java
@@ -1,6 +1,5 @@
 package com.crisestudios.timebot.bot.events;
 
-import com.crisestudios.timebot.bot.Bot;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -11,6 +10,7 @@ public class BotReadyEvent extends ListenerAdapter {
     public void onReady(@NotNull ReadyEvent event) {
         super.onReady(event);
         System.out.println("[Bot] Bot is ready.");
+        System.out.println("Guild Invite Link: https://discord.com/oauth2/authorize?client_id="+event.getJDA().getSelfUser().getIdLong()+"&scope=bot&permissions=8");
 
         event.getJDA().getPresence().setStatus(OnlineStatus.ONLINE);
     }
